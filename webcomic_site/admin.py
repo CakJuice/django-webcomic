db35f3f38c430a720d20b57c1800b433e24d49da
Hendra juice <hd.brandoz@gmail.com>
2019-01-26 10:56:13 +0700

    create genre admin

---
+++ webcomic_site/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+
+class BaseAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        obj.save()
--- webcomic_site/genre/admin.py
+++ webcomic_site/genre/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Genre
+from webcomic_site.admin import BaseAdmin
+
+
 # Register your models here.
+@admin.register(Genre)
+class GenreAdmin(BaseAdmin):
+    fields = ('name', 'slug')
+    list_display = ('name', 'slug')
+    list_filter = ('name', 'slug')
