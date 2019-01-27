6d900e9cb1ab699c2644bad9e012556e3064939a
Hendra juice <hd.brandoz@gmail.com>
2019-01-27 10:32:18 +0700

    add signup function

--- webcomic_site/base/urls.py
+++ webcomic_site/base/urls.py
@@ -9,6 +9,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- webcomic_site/base/views.py
+++ webcomic_site/base/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+
+from .forms import SignupForm
 
 
 # Create your views here.
 def homepage(request):
     """ Display homepage request.
-    :param request: User request.
+    :param request: Page request.
     :return: Renderer page for user with template as param.
     """
     return render(request, 'base/homepage.html')
+
+
+def signup(request):
+    """ Handle signup/registration for new user.
+    :param request: Page request.
+    :return: Renderer page for user with template and context as params.
+    """
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('homepage')
+    else:
+        form = SignupForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'base/signup.html', context)
