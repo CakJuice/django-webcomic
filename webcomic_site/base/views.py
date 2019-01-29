2cf707791c49a754dd6e1e0a3c23b22767cf3a52
Hendra Juice <hd.brandoz@gmail.com>
2019-01-29 14:15:43 +0700

    create user activation

--- webcomic_site/base/views.py
+++ webcomic_site/base/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 
+from .models import UserActivation
 from .forms import SignupForm
 
 
@@ -24,6 +25,8 @@ def signup(request):
             user = form.save(commit=False)
             user.is_active = False
             user.save()
+            activation = UserActivation.objects.create(user=user)
+            activation.save()
             return redirect('signup_success', username=user.username)
     else:
         form = SignupForm()
@@ -43,3 +46,11 @@ def signup_success(request, username):
     """
     user = get_object_or_404(User, username=username)
     return render(request, 'base/signup_success.html', context={'email': user.email})
+
+
+def user_activation(request, token):
+    activation = get_object_or_404(UserActivation, token=token)
+    if activation.activated():
+        return render(request, 'base/user_activation_success.html')
+
+    return render(request, 'base/user_activation_expire.html')
--- webcomic_site/user/models.py
+++ webcomic_site/user/models.py
@@ -1,31 +1,28 @@
-from datetime import datetime
+from datetime import timedelta
 
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.crypto import get_random_string
 
 
 # Create your models here.
 class UserActivation(models.Model):
     user = models.ForeignKey(User, on_delete='CASCADE', related_name='activations', verbose_name="User")
-    token = models.CharField(max_length=32, verbose_name="Token", null=True)
+    token = models.CharField(max_length=32, verbose_name="Token")
     is_activated = models.BooleanField(verbose_name="Is Activated", default=False)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
-    expire_at = models.DateTimeField(verbose_name="Expire At", null=True)
+    expire_at = models.DateTimeField(verbose_name="Expire At")
 
     class Meta:
         db_table = settings.DATABASE_TABLE_PREFIX + 'user_activation'
-        ordering = ('-created_at',)
+        ordering = ('-created_name',)
 
     def __str__(self):
         return '%s - %s' % (self.user, self.token)
 
-    def activated(self):
-        if datetime.now() < self.expire_at:
-            return False
-
-        self.is_activated = True
-        self.save()
-        self.user.is_active = True
-        self.user.save()
-        return True
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = get_random_string(32)
+        if not self.expire_at:
+            self.expire_at = self.created_at + timedelta(days=settings.EXPIRE_USER_ACTIVATION)
