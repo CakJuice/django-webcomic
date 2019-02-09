bdf29f0b41f7d58d2121832c30779b0d1dcd36d8
Hendra juice <hd.brandoz@gmail.com>
2019-02-09 20:14:22 +0700

    reset password

--- webcomic_site/base/urls.py
+++ webcomic_site/base/urls.py
@@ -14,4 +14,15 @@ urlpatterns = [
     path('activation/<token>/', views.user_activation, name='user_activation'),
     path('login/', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('reset/', auth_views.PasswordResetView.as_view(
+        template_name='base/password_reset.html',
+        email_template_name='base/password_reset_email.html',
+        subject_template_name='base/password_reset_subject.txt'
+    ), name='password_reset'),
+    path('reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='base/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='base/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
 ]
