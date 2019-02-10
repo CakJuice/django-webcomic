3e2c1af2ca635f8e1e3be2891ddca7e835456899
Hendra juice <hd.brandoz@gmail.com>
2019-02-10 18:51:45 +0700

    create password_reset_complete

--- webcomic_site/base/urls.py
+++ webcomic_site/base/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='base/password_reset_confirm.html'
     ), name='password_reset_confirm'),
+    path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='base/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
