b2ad31d98676194b585d6f80447ea8ea37405725
Hendra juice <hd.brandoz@gmail.com>
2019-02-14 05:52:50 +0700

    bug fix comic create

--- webcomic_site/comic/views.py
+++ webcomic_site/comic/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic.edit import CreateView
 
@@ -17,8 +19,14 @@ def genre_detail(request, slug):
 
 # check https://docs.djangoproject.com/en/2.1/topics/class-based-views/intro/ for decorating the class
 @method_decorator(login_required, name='dispatch')
-class ComicCreate(CreateView):
+class ComicCreate(CreateView, SuccessMessageMixin):
     model = Comic
-    fields = ('title', 'description', 'genre', 'slug',)
+    fields = ('title', 'description', 'genre',)
     template_name = 'comic/create.html'
     context_object_name = 'form'
+    success_message = 'Congratulation! You have created a new comic.'
+    success_url = reverse_lazy('homepage')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
