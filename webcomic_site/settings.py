48b47c7aa3d889af873e9fb381ddb442434689a3
Hendra Juice <hd.brandoz@gmail.com>
2019-03-21 15:49:36 +0700

    bug fix comic detail template

--- webcomic_site/settings.py
+++ webcomic_site/settings.py
@@ -64,7 +64,6 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                # 'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'webcomic_site.context_processors.default',
