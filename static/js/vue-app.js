63d82b308468f572c3dbdcee9341f80be362b943
Hendra juice <hd.brandoz@gmail.com>
2019-04-15 13:45:47 +0700

    change display comic list from native to vuejs component

--- static/js/vue-app.js
+++ static/js/vue-app.js
@@ -1,3 +1,56 @@
+// ListDataAPI used for creating data list from API consume.
+// It will get data with AJAX then passing to list item and list pagination.
+// It needs `url` props to passing URL of API consume.
+var ListDataAPI = {
+  props: ['url'],
+  data: function() {
+    return {
+      items: [],
+      pagination: {
+        previous: null,
+        next: null,
+      },
+    }
+  },
+  computed: {
+    baseUrl: function() {
+      return this.url.split(/[?#]/)[0];
+    }
+  },
+  methods: {
+    fetchData: function(url) {
+      var self = this;
+      console.log('parent fetch data', self);
+      axios.get(url).then(function(response) {
+        self.items = response.data.results;
+        self.pagination = {
+          count: response.data.count,
+          current: response.data.current,
+          end: response.data.end,
+          next: response.data.next,
+          numPages: response.data.num_pages,
+          previous: response.data.previous,
+          start: response.data.start,
+        };
+      });
+    },
+  },
+  mounted: function() {
+    this.fetchData(this.url);
+  },
+}
+
+// ListItem used in container item of data list.
+var ListItem = {
+  props: ['items'],
+  computed: {
+    defaultThumbnail: function() {
+      return DEFAULT_THUMBNAIL
+    }
+  },
+}
+
+// List pagination used for all pagination purpose.
 Vue.component('list-pagination', {
   props: ['pagination', 'baseUrl'],
   computed: {
@@ -18,28 +71,28 @@ Vue.component('list-pagination', {
         '<span class="page-link">Page {{ pagination.current }} Of {{ pagination.numPages }}</span>' +
       '</li>' +
       '<li class="page-item" v-if="pagination.previous">' +
-        '<a class="page-link" :href="baseUrl + \'?page=1\'">&laquo;</a>' +
+        '<a class="page-link" href="#" @click.prevent="clickPage(baseUrl + \'?page=1\')">&laquo;</a>' +
       '</li>' +
       '<li class="page-item" :class="{ active: page.isActive }" v-for="page in pages" :key="page.val">' +
         '<span class="page-link" v-if="page.isActive">' +
           '{{ page.val }} <span class="sr-only">(current)</span>' +
         '</span>' +
-        '<a class="page-link" v-else :href="baseUrl + \'?page=\' + page.val">{{ page.val }}</a>' +
+        '<a class="page-link" href="#" v-else @click.prevent="clickPage(baseUrl + \'?page=\' + page.val)">{{ page.val }}</a>' +
       '</li>' +
       '<li class="page-item" v-if="pagination.next">' +
-        '<a class="page-link" :href="baseUrl + \'?page=\' + pagination.numPages">&raquo;</a>' +
+        '<a class="page-link" href="#" @click.prevent="clickPage(baseUrl + \'?page=\' + pagination.numPages)">&raquo;</a>' +
       '</li>' +
     '</ul>' +
   '</nav>',
+  methods: {
+    clickPage: function(url) {
+      this.$parent.fetchData(url);
+    },
+  },
 });
 
 Vue.component('author-chapter-list', {
-  props: ['items'],
-  computed: {
-    defaultThumbnail: function() {
-      return DEFAULT_THUMBNAIL
-    }
-  },
+  extends: ListItem,
   template: '<div class="row">' +
     '<div class="col col-md-6 col-12" v-for="item in items" :key="item.slug">' +
       '<div class="row my-2 mx-1 py-2 chapter-list">' +
@@ -54,38 +107,37 @@ Vue.component('author-chapter-list', {
 });
 
 Vue.component('author-chapter-container', {
-  props: ['url'],
-  data: function() {
-    return {
-      items: [],
-      pagination: {
-        previous: null,
-        next: null,
-      },
-    }
-  },
-  computed: {
-    baseUrl: function() {
-      return this.url.split(/[?#]/)[0];
-    }
-  },
+  extends: ListDataAPI,
   template: '<div>' +
     '<author-chapter-list v-if="items.length > 0" :items="items"></author-chapter-list>' +
-    '<list-pagination v-if="pagination.previous && pagination.next" :pagination="pagination" :baseUrl="baseUrl"></list-pagination>' +
+    '<list-pagination v-if="pagination.previous || pagination.next" :pagination="pagination" :baseUrl="baseUrl"></list-pagination>' +
   '</div>',
-  mounted: function() {
-    var self = this;
-    axios.get(this.url).then(function(response) {
-      self.items = response.data.results;
-      self.pagination = {
-        count: response.data.count,
-        current: response.data.current,
-        end: response.data.end,
-        next: response.data.next,
-        numPages: response.data.num_pages,
-        previous: response.data.previous,
-        start: response.data.start,
-      };
-    });
+});
+
+Vue.component('comic-list', {
+  extends: ListItem,
+  template: '<div class="row">' +
+    '<div class="col col-lg-4 col-md-6 col-12" v-for="item in items" :key="item.slug" @click="redirectUrl(item.direct_url)">' +
+      '<div class="row my-2 mx-1 py-2 comic-list">' +
+        '<div class="col col-4 pr-1">' +
+          '<img :src="item.thumbnail" v-if="item.thumbnail" class="img-fluid" loading="lazy">' +
+          '<img :src="defaultThumbnail" v-else class="img-fluid" loading="lazy">' +
+        '</div>' +
+        '<div class="col col-8 pl-1"><h5>{{ item.title }}</h5></div>' +
+      '</div>' +
+    '</div>' +
+  '</div>',
+  methods: {
+    redirectUrl: function(url) {
+      window.location.href = url;
+    }
   },
+});
+
+Vue.component('comic-list-container', {
+  extends: ListDataAPI,
+  template: '<div>' +
+    '<comic-list v-if="items.length > 0" :items="items"></comic-list>' +
+    '<list-pagination v-if="pagination.previous || pagination.next" :pagination="pagination" :baseUrl="baseUrl"></list-pagination>' +
+  '</div>',
 });
